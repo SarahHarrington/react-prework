5bb13cfc2423c9214e9ff7fa754af2e34923e2d5
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-01 17:44:16 -0500

    updated per lecture, created array, currying, data binding inputs

--- react-star-lists/src/Components/App/App.js
+++ react-star-lists/src/Components/App/App.js
@@ -5,24 +5,104 @@ class App extends Component {
     super(props);
 
     this.state = {
-      starList: ['Hadar', 'Gacrux', 'FomalHaut']
+      starList: [
+        {name: 'Hadar', diameter: 50}, 
+        {name: 'Gacrux', diameter: 75}, 
+        {name: 'FomalHaut', diameter: 100}
+      ],
+      newStar: {
+        name: '',
+        diameter: ''
+      }
+    }
+
+    // this.handleNameChange = this.handleNameChange.bind(this);
+    // this.handleDiameterChange = this.handleDiameterChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // handleNameChange(event) {
+  //   this.setState({
+  //     newStar: {
+  //       ...this.state.newStar,
+  //       name: event.target.value,
+  //     }
+  //   })
+  // }
+
+  // handleDiameterChange(event) {
+  //   this.setState({
+  //     newStar: {
+  //       ...this.state.newStar,
+  //       diameter: event.target.value
+  //     }
+  //   })
+  // }
+
+  handleChangeFor(propertyName) {
+    return(event) => {
+      this.setState({
+        newStar: {
+          ...this.state.newStar,
+          [propertyName]: event.target.value
+        }
+      })
     }
   }
 
-  render() {
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('button clicked');
+    console.log(this.state.newStar)
+    this.setState({
+      starList: [...this.state.starList, this.state.newStar],
+      newStar: {
+        name: '',
+        diameter: ''
+      }
+    })
+  }
 
-    const newStarList = this.state.starList.map()
+  render() {
+    
+    //ES6 one line function
+    // const starListItemArray = this.state.starList.map(star => <li>{star}</li>);
+
+    // let starListItemArray = [];
+    
+    //for each loop
+    // this.state.starList.forEach(function(star){
+    //   const starListItem = <li>{star}</li>
+    //   starListItemArray.push(starListItem)
+    // })
+
+    //traditional for loop
+    // for (let i = 0; i < this.state.starList.length; i++) {
+    //   const starListItem = <li>{this.state.starList[i]}</li>
+    //   starListItemArray.push(starListItem)
+    // }
 
     return (
       <div>
-        <div>
+        {/* <div>
           this.state: {JSON.stringify(this.state)}
         </div>
         <div>
-          this.state.starList: { this.state.starList }
+          {/* this.state.starList: { this.state.starList } */}
           {/* React can display arrays directly to the DOM */}
+        {/* </div> */}
+        <div>
+          <div>
+            Current New Star is { this.state.newStar.name } with a diamter of {this.state.newStar.diameter}.
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChangeFor('name')} value={this.state.newStar.name}/>
+            <input onChange={this.handleChangeFor('diameter')} value={this.state.newStar.diameter}/>
+            <input type='submit' value='Submit New Star'/>
+          </form>
         </div>
         <ul>
+          {this.state.starList.map(star => <li key={star.name} >This star has a {star.name} and has a diamter of {star.diameter} </li>)}
         </ul>
       </div>
     );
