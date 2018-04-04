0dd2dc8ca36cb6491e774f785bf859beee1d7571
Sarah Harrington <sarah.soberg@gmail.com>
2018-04-04 13:42:33 -0500

    updated notes in comments

--- generator-function/ready-set-go.js
+++ generator-function/ready-set-go.js
@@ -22,25 +22,25 @@ console.log('generator functions lecture');
 //generator functions have a yield rather than a return
 //we can yield pretty much anything from a generator function
 //the generator will remember where it left off
-function* firstGenerator() {
-  yield 1;
-  yield 'Hello World';
-  yield { name: 'Sarah' };
-}
+// function* firstGenerator() {
+//   yield 1;
+//   yield 'Hello World';
+//   yield { name: 'Sarah' };
+// }
 
-function* readySetGo() {
-  yield delayPromiseWord('Ready');
-  yield delayPromiseWord('Set');
-  yield delayPromiseWord('Go');
-}
+// function* readySetGo() {
+//   yield delayPromiseWord('Ready');
+//   yield delayPromiseWord('Set');
+//   yield delayPromiseWord('Go');
+// }
 
-//resolve is a function that is going to run to tell our promise that it's done
-const delayPromiseWord = word => new Promise(resolve => {
-  setTimeout(() => {
-    console.log(word);
-    resolve(); //kind of like a done
-  }, 1000);
-});
+// //resolve is a function that is going to run to tell our promise that it's done
+// const delayPromiseWord = word => new Promise(resolve => {
+//   setTimeout(() => {
+//     console.log(word);
+//     resolve(); //kind of like a done
+//   }, 1000);
+// });
 
 //to call the function you need to create an instance of it
 // const firstGeneratorInstance = firstGenerator();
@@ -78,21 +78,59 @@ const runEntireGenerator = (generatorFunc) => {
 //   }
 // }
 
-const recursiveNext = (generatorFuncInstance) => {
-  let generatorResponse = generatorFuncInstance.next();
-  if (generatorResponse.done) { //basically checking to see if done is returning true
+// const recursiveNext = (generatorFuncInstance) => {
+//   let generatorResponse = generatorFuncInstance.next();
+//   if (generatorResponse.done) { //basically checking to see if done is returning true
+//     // do nothing, no more code to run
+//   } 
+//   else if (typeof generatorResponse.value.then === 'function') { //this checks to see if it's a promise
+//     generatorResponse.value.then(() => { //waits for the function to complete and then does .then to do the recursiveNext
+//       recursiveNext(generatorFuncInstance);
+//     })
+//   }
+//   else {
+//     console.log(generatorResponse.value); 
+//     recursiveNext(generatorFuncInstance); //calls again with the same instance, keeping track of where the instance is
+//   }
+// }
+
+// // runEntireGenerator(firstGenerator);
+// runEntireGenerator(readySetGo);
+
+
+function* readySetGo() {
+  yield delayPromiseWord('Ready');
+  yield delayPromiseWord('Set');
+  yield delayPromiseWord('Go');
+}
+
+const delayPromiseWord = word => new Promise(resolve => {
+  setTimeout(() => {
+    console.log(word);
+    resolve(); 
+  }, 1000);
+});
+
+const runEntireGenerator = (generatorFunc) => { //calls the function 
+  const generatorFuncInstance = generatorFunc(); //creates an instance of the function
+  recursiveNext(generatorFuncInstance); // calls the recursive function with the instance of the function as the parameter
+}
+
+const recursiveNext = (generatorFuncInstance) => { //this function takes in the instance of the orginal function
+  let generatorResponse = generatorFuncInstance.next(); //the response variable is set to the result of the instance.next - this
+  if (generatorResponse.done) { // checks to see if the function comes up as true - if it does, it does nothing
     // do nothing, no more code to run
   } 
-  else if (typeof generatorResponse.value.then === 'function') { //this checks to see if it's a promise
-    generatorResponse.value.then(() => { //waits for the function to complete and then does .then to do the recursiveNext
-      recursiveNext(generatorFuncInstance);
+  else if (typeof generatorResponse.value.then === 'function') { //checks if the response is a function, if it is it 
+    generatorResponse.value.then(() => { //waits for the result of the function and does a .then to start the next iteration
+      recursiveNext(generatorFuncInstance); //calls the next iteration for recursive next on the original function instance yield
     })
   }
-  else {
+  else { // if it's not a function, it will just show the result of the function instance yield
     console.log(generatorResponse.value); 
-    recursiveNext(generatorFuncInstance); //calls again with the same instance, keeping track of where the instance is
+    recursiveNext(generatorFuncInstance); 
   }
 }
 
-// runEntireGenerator(firstGenerator);
-runEntireGenerator(readySetGo);
+runEntireGenerator(readySetGo); // this calls the function with readySetGo as the parameter
+
